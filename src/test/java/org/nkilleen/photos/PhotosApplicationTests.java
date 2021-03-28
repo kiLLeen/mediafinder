3792676dea4535ca3c0265a1101a84ffa23655b2
Neil Killeen <nkilleen@gmail.com>
2021-03-28 13:34:35 -0700

    + Initial commit

---
+++ src/test/java/org/nkilleen/photos/PhotosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nkilleen.photos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
